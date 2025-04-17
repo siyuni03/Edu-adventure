dfecdb2cf5812db6d3e46fbd23fb17ade266d2f3
siyuni03 <ericamansea@gmail.com>
2025-04-17 14:16:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
+document.getElementById("limit-forest").addEventListener("mouseover", function () {
+  alert("극한의 숲: 수렴과 발산으로 변하는 숲 속에서 길을 찾아보자!");
+});
 
-window.onload = () => {
-    const minkyu = document.getElementById('minkyu');
-    const speech = document.getElementById('speech');
-
-    setTimeout(() => {
-        minkyu.classList.add('show');
-    }, 500);
-
-    setTimeout(() => {
-        speech.classList.add('show');
-    }, 2000);
-};
+document.getElementById("limit-forest").addEventListener("click", function () {
+  window.location.href = "limit-forest.html";
+});
