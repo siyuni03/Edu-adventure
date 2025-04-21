3175440fc92ddfb2a9b395ab0b27093e29fb7158
siyuni03 <ericamansea@gmail.com>
2025-04-21 19:40:11 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import MinguDialog from './components/MinguDialog';
+
+export default function App() {
+  return (
+    <div className="w-full h-screen bg-blue-100 relative">
+      <MinguDialog />
+    </div>
+  );
+}
