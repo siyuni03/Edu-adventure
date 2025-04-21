7c89723c77f75d3bee82baa47391acff0731bd2d
siyuni03 <ericamansea@gmail.com>
2025-04-21 19:39:42 +0900

    Delete mingu-dialog-demo directory

--- mingu-dialog-demo/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import MinguDialog from './components/MinguDialog';
-
-export default function App() {
-  return (
-    <div className="w-full h-screen bg-blue-100 relative">
-      <MinguDialog />
-    </div>
-  );
-}
